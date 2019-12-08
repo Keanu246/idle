1249e296f8d820ae08997af87dfb28a14df4109a
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-12-08 23:56:30 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 //let cup = new idle.upgrade("name", price, money per click, money per second, money per click multiplier, cost multiplier)
 //For now, the "name" and the object name (let name = new idle.upgrade()) need to be the same (and the object must be lowercase… aaaaand it can only be 1 word for now)
-let cursor = new idle.upgrade("Cursor", 1, 0.01, 0.01, 1, 1.1, 1.3);
-let grandma = new idle.upgrade("Grandma", 10, 0.05, 0.05, 0, 1.1, 1.3);
+let gun = new idle.upgrade("Gun", 1, 0.01, 0.01, 1, 1.05, 1.1);
+let powergun = new idle.upgrade("Power Gun", 10, 0.05, 0.05, 1, 1.1, 1.5);
+let rifle = new idle.upgrade("Rifle", 100, 0.4, 0.4, 1, 1.15, 1.4);
+let assaultrifle = new idle.upgrade("Assault Rifle", 1000, 3.5, 3.5, 1, 1.2, 1.3);
+let shotgun = new idle.upgrade("Shotgun", 10000, 30, 30, 1, 1.25, 1.25);
 
