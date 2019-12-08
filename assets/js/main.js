573f9f7baceec84cfa1ac29e4668d4238eaab832
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-12-08 23:47:35 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
 //let cup = new idle.upgrade("name", price, money per click, money per second, money per click multiplier, cost multiplier)
 //For now, the "name" and the object name (let name = new idle.upgrade()) need to be the same (and the object must be lowercase… aaaaand it can only be 1 word for now)
-let test = new idle.upgrade("Test", 1, 0.01, 0, 1, 1.1, 1.3);
+let cursor = new idle.upgrade("Cursor", 1, 0.01, 0.01, 1, 1.1, 1.3);
+let grandma = new idle.upgrade("Grandma", 10, 0.05, 0.05, 0, 1.1, 1.3);
+
